4764dedc41e6428e883a37e0ef6d1e5109a58143
unknown <televisiondart@gmail.com>
2019-08-26 00:30:48 -0400

    Shiny support and made images nicer

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 
-var id; // ID number of the Pokemon that was chosen
-var name; // String name of the Pokemon
-var pokemonNames; // JSON Array of the Pokemon names
+var id;
+var name;
+var pokemonNames;
+var isShiny;
 
 function readJson (callback) {
   // Grabs the JSON file containing the data of all the Pokemon.
@@ -22,9 +23,10 @@ function readJson (callback) {
 
 
 
-// Generates the image when ran.
+// Generates the image when ran
 function generateImage(jsonFile) {
-  id = (Math.floor(Math.random() * 250) + 1);
+  isShiny = (Math.floor(Math.random() * 100) + 1);
+  id = (Math.floor(Math.random() * 386) + 1);
   pokemonNames = jsonFile;
   name = jsonFile[id-1];
 
@@ -34,30 +36,43 @@ function generateImage(jsonFile) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // Get BG image
   var img = new Image();
-  img.src = 'images/sprites/' + id + '.png';
+  
+  if (isShiny >= 95) {
+    img.src = 'images/sprites/shiny/' + id + '.png';
+    name = 'Shiny ' + name;
+  } else {
+    img.src = 'images/sprites/' + id + '.png';
+  }
+  
   img.onload = function() {
-    // Generate Text
+    // Generate Background
+    var lingrad = ctx.createLinearGradient(0, 0, 0, 150);
+    lingrad.addColorStop(0, '#00ABEB');
+    lingrad.addColorStop(0.5, '#fff');
+    lingrad.addColorStop(0.5, '#26C000');
+    lingrad.addColorStop(1, '#fff');
+    ctx.fillStyle = lingrad;
 
+    // Generate Text
+      ctx.textAlign = "center";
       ctx.font = '20px Noto Sans HK';
       ctx.fillStyle = 'Black';
-      ctx.fillText(name, 0, 15);
+      ctx.fillText(name, canvas.width/2, 20);
       ctx.font = '15px Noto Sans HK';
-      ctx.fillText('Level ' + (Math.floor(Math.random() * 100) + 1), 0, 35);
-      ctx.drawImage(img, 0, 50);
+      ctx.fillText('Level ' + (Math.floor(Math.random() * 100) + 1), canvas.width/2, 40);
+      ctx.drawImage(img, canvas.width / 2 - img.width / 2, 55);
   }
-}
-
-// Function that runs when the "Generate Image" button is pressed.
-function buttonPress() {
-  var canvas = document.getElementById('tutorial');
-  var ctx = canvas.getContext('2d');
-  // Clears the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  generateImage(pokemonNames);
 }
 
 // When the page loads, run the readJson function, then run generateImage when it's done
 readJson(function (result) {
   generateImage(result);
 });
+
+function buttonPress() {
+  var canvas = document.getElementById('tutorial');
+  var ctx = canvas.getContext('2d');
+  // Clears the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  generateImage(pokemonNames);
+}
